e20fee97c22dd624dbc8058ada697bcfb1d4db0c
Julia <julia@Julias-MacBook-Pro.local>
2020-12-23 12:04:47 +0100

    Removed files

--- app.py
+++ app.py
@@ -3,21 +3,6 @@ import os
 from os.path import join, dirname, realpath
 app = Flask(__name__)
 
-
-@app.route('/myroute', methods=['POST'])
-def myroute():
-   flask_file = request.files['file']
-   if not flask_file:
-      return 'Upload a CSV file'
-
-   data = []
-   stream = codecs.iterdecode(flask_file.stream, 'utf-8')
-   for row in csv.reader(stream, dialect=csv.excel):
-      if row:
-         data.append(row)
-
-   return jsonify(data)
-
 @app.route('/')
 def home():
    return render_template('template.html')
