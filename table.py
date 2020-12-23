e5a36655ca65f633e424b2a616407994f6530e8d
Julia <julia@Julias-MacBook-Pro.local>
2020-12-23 13:51:59 +0100

    Uodating styles and corect display table

--- table.py
+++ table.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/tables")
 def show_tables():
     data = pd.read_csv('static/css/LINE.csv',',')
-    return render_template('template.html', tables=[data.to_html(classes='data')], titles=data.columns.values)
+    #data.set_index(['ID'], inplace=True)
+    #data.index.id = None
+    return render_template('template.html', tables=[data.to_html(classes='data', index=False)], titles=data.columns.values)
 
 
 if __name__ == "__main__":
